e4e52c6ff6002e199019fb3f357cf5be3aecb0d8
niandra-l <185798403+niandra-l@users.noreply.github.com>
2025-02-04 16:02:50 +0100

    Change the play again page, added final score

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 let humanScore=0;
 let computerScore=0;
 
-const startBtn = document.querySelector("#startBtn");
+const startBtn = document.querySelector("#start-button");
 const selectionBtn = document.querySelectorAll(".selection-button");
 const buttonsContainer = document.querySelector(".buttons-container");
 
 const computerMove = document.querySelector("#computer-move");
 const humanMove = document.querySelector("#human-move");
 
-const uiContainer = document.querySelector("#ui-container");
-const startContainer = document.querySelector("#start-container");
+const uiContainer = document.querySelector(".ui-container");
+const startContainer = document.querySelector(".start-container");
 
-const results = document.querySelector("#results");
 const roundResult = document.querySelector("#round-result");
 const finalResult = document.querySelector("#final-result");
-const playAgainCont = document.querySelector("#play-again");
+const totalScore = document.querySelector("#total-score");
+const summary = document.querySelector(".summary");
 const playAgainBtn = document.querySelector("#play-again-button");
 
 const yourScore = document.querySelector("#your-score");
 const compScore = document.querySelector("#comp-score");
+const yourFinalScore = document.querySelector("#your-final-score");
+const compFinalScore = document.querySelector("#comp-final-score");
 
 uiContainer.style.display = "none"; 
-playAgainCont.style.display = "none";
+summary.style.display = "none";
 
 function playGame() {
     startBtn.addEventListener("click", () => {
@@ -35,7 +37,6 @@ playGame();
 selectionBtn.forEach(button => {
     button.addEventListener("click",  (e) => {
     let playerChoice = e.target.id;
-    humanMove.textContent = "Your choice: " + playerChoice;
     playRound(playerChoice, getComputerChoice());
        
      });
@@ -72,7 +73,7 @@ function playRound(humanSelection, computerSelection) {
                 roundResult.textContent = "You lose! Paper beats rock!";
                 ++computerScore
             } else if (computerSelection==="scissors") {
-                roundResult.textContent = "You win! Rock beats scissors";                
+                roundResult.textContent = "You win! Rock beats scissors.";                
                 ++humanScore
             }
             break;
@@ -84,17 +85,17 @@ function playRound(humanSelection, computerSelection) {
                 roundResult.textContent = "You win! Paper beats rock!";                
                 ++humanScore
             } else if (computerSelection==="scissors") {
-                roundResult.textContent = "You lose! Scissors beat paper";
+                roundResult.textContent = "You lose! Scissors beat paper.";
                 ++computerScore
             }
             break;
 
         case "scissors":
             if (computerSelection ==="rock") {
-                roundResult.textContent = "You lose! Rock beats scissors";
+                roundResult.textContent = "You lose! Rock beats scissors.";
                 ++computerScore
             } else if (computerSelection==="paper"){
-                roundResult.textContent = "You win! Scissors beat paper";
+                roundResult.textContent = "You win! Scissors beat paper.";
                 ++humanScore
             } else if (computerSelection==="scissors") {
                 roundResult.textContent = "It's a tie!";                
@@ -108,13 +109,20 @@ function playRound(humanSelection, computerSelection) {
 
  function declareWinner() {
     if(humanScore === 5) {
+        uiContainer.style.display = "none";
+        summary.style.display = "initial";
+        yourFinalScore.textContent = "Your score: " + humanScore;
+        compFinalScore.textContent = "Computer score: " + computerScore;
         finalResult.textContent = "Congratulations! You won the game!";
-        buttonsContainer.style.display = "none";
-        playAgainCont.style.display = "initial";  
+        finalResult.setAttribute = "color: #4c9f4f";
+          
     } else if (computerScore === 5) {
-        finalResult.textContent = "You lost the game! Better luck next time."
-        buttonsContainer.style.display = "none";
-        playAgainCont.style.display = "initial"; 
+        uiContainer.style.display = "none";
+        summary.style.display = "initial"; 
+        yourFinalScore.textContent = "Your score: " + humanScore;
+        compFinalScore.textContent = "Computer score: " + computerScore;
+        finalResult.textContent = "You lost the game! Better luck next time.";
+        finalResult.setAttribute = "color: #f46363";
     } 
 };
 
@@ -123,14 +131,14 @@ playAgainBtn.addEventListener("click", playAgain);
 function playAgain() {
     humanScore=0;
     computerScore=0;
-    computerMove.textContent = "Computer choice: ";
-    humanMove.textContent = "Your choice: ";
+    uiContainer.style.display = "initial";
     yourScore.textContent = "Your score: " + humanScore;
     compScore.textContent = "Computer score: " + computerScore;
     roundResult.textContent = "";
     finalResult.textContent = "";
-    buttonsContainer.style.display = "initial";
-    playAgainCont.style.display = "none";
+    summary.style.display = "none";
+    
+    
 };
 
 
